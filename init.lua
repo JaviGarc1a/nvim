4f0978f7a5d59111c06cd4518077779f08bb568a
JaviGarc1a <javgaragu1@alum.us.es>
2024-11-07 20:01:15 +0100

    Initial commit

---
+++ init.lua
@@ -0,0 +1,2 @@
+require('markdown-preview')
+require('plugins.plugins')
---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,38 @@
+-- Autocommand that reloads neovim whenever you save this file
+vim.cmd([[
+augroup packer_user_config
+autocmd!
+autocmd BufWritePost plugins.lua source <afile> | PackerSync
+augroup end
+]])
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+	return
+end
+
+-- Have packer use a popup window
+packer.init({
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
+})
+
+return require('packer').startup(function(use)
+	-- Essentials
+	use('wbthomason/packer.nvim')
+
+	-- MARKDOWN
+
+	use({
+		"iamcco/markdown-preview.nvim",
+		run = "cd app && npm install",
+		setup = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	})
+end)
