0ae71a286182f2f7448cafd222ff1bc23ba1ab58
JaviGarc1a <javgaragu1@alum.us.es>
2024-11-26 13:21:00 +0100

    conf: Change tab widthi from 2 to 4 spaces

--- lua/options/init.lua
+++ lua/options/init.lua
@@ -1,6 +1,7 @@
 vim.wo.relativenumber = true
 vim.cmd('set expandtab')
-vim.cmd('set tabstop=2')
-vim.cmd('set softtabstop=2')
-vim.cmd('set shiftwidth=2')
+vim.cmd('set tabstop=4')
+vim.cmd('set softtabstop=4')
+vim.cmd('set shiftwidth=4')
 vim.g.mapleader = ' '
+vim.cmd.colorscheme "catppuccin"
