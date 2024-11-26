fbbfd034e068e90219dc7ef5ebb7029e2c5adecd
JaviGarc1a <javgaragu1@alum.us.es>
2024-11-26 14:27:54 +0100

    conf: Add catppuccin theme

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -26,6 +26,7 @@ return require('packer').startup(function(use)
 	use('wbthomason/packer.nvim')
 	use({ "nvim-treesitter/nvim-treesitter", run = ':TSUpdate' })
 
+  use { "catppuccin/nvim", as = "catppuccin" }
 	-- Extensions
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.8',
