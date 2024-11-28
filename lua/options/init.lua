1a474ea0510ce9d944c32664b4859a32bdfe9566
JaviGarc1a <javgaragu1@alum.us.es>
2024-11-28 13:31:04 +0100

    conf: Config number line hybrid

--- lua/options/init.lua
+++ lua/options/init.lua
@@ -3,5 +3,6 @@ vim.cmd('set expandtab')
 vim.cmd('set tabstop=4')
 vim.cmd('set softtabstop=4')
 vim.cmd('set shiftwidth=4')
+vim.cmd('set number relativenumber')
 vim.g.mapleader = ' '
-vim.cmd.colorscheme "catppuccin"
+
