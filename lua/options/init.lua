7df1ba61cdeaa42ed47bfd9b57ab309a43e9bc80
JaviGarc1a <javgaragu1@alum.us.es>
2024-12-04 11:57:30 +0100

    conf: Config keymaps

--- lua/options/init.lua
+++ lua/options/init.lua
@@ -5,4 +5,4 @@ vim.cmd('set softtabstop=4')
 vim.cmd('set shiftwidth=4')
 vim.cmd('set number relativenumber')
 vim.g.mapleader = ' '
-
+vim.api.nvim_set_keymap('n', '<leader>q', ':cclose<CR>:lclose<CR>', { noremap = true, silent = true })
