f7341aab680a406a862d3ecb0bdd662c9ce69d1c
JaviGarc1a <javgaragu1@alum.us.es>
2025-07-26 18:00:41 +0200

    fix: Fix name events

--- lua/plugins/lsp/autotags.lua
+++ lua/plugins/lsp/autotags.lua
@@ -8,7 +8,7 @@ return {
 		"typescriptreact",
 		"astro",
 	},
-	events = { "ButReadPre", "ButNewFile" },
+	events = { "BufReadPre", "BufNewFile" },
 	config = function()
 		require("nvim-ts-autotag").setup()
 	end,
