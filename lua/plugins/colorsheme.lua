a21e27589d88dc04cab1256b9312288865ea2a20
Javier García Aguilar <javier.garcia@skylife-eng.com>
2025-11-17 14:22:36 +0100

    feat: Change cursor line on neotree

--- lua/plugins/colorsheme.lua
+++ lua/plugins/colorsheme.lua
@@ -1,10 +1,18 @@
 return {
 	"catppuccin/nvim",
 	name = "catppuccin",
+	-- lazy = true,
+	-- event = "VeryLazy",
 	priority = 1000,
 	opts = function()
 		require("catppuccin").setup({
 			transparent_background = true,
+			auto_integrations = true,
+			custom_highlights = function(colors)
+				return {
+					NeoTreeCursorLine = { bg = colors.surface1, bold = true }
+				}
+			end
 		})
 		vim.cmd.colorscheme("catppuccin-mocha")
 	end,
